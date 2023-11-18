6fe82d93b430b5f1e68066910bdbeeb85aaa5c56
jacques <jacques.carlier@gmail.com>
2023-11-18 10:20:40 +0100

    creation of the feature component

--- argentbank-front/src/pages/Home.jsx
+++ argentbank-front/src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import Banner from "../containers/banner/Banner";
-import Features from "../containers/features/Features";
+import Features from "../components/features/Features";
 import Header from "../containers/nav/Nav";
+import { featuresData } from "../data/data"
+import "../css/home.css"
 
 export default function Home() {
+
   return (
     <>
       <Header />
       <main>
         <Banner />
-        <Features />
+        <section className="features">
+          <h2 className="sr-only">Features</h2>
+          {featuresData.map((feature) => (
+            <Features
+              key={feature.title}
+              title={feature.title}
+              content={feature.content}
+              imageSrc={feature.imageSrc}
+              altText={feature.altText}
+            />))}
+        </section>
       </main>
     </>
   );
 }
-
