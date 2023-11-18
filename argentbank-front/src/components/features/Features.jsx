a7a90ed2b085574c30deb41a1f9bf8ba9c54875f
jacques <jacques.carlier@gmail.com>
2023-11-18 10:55:55 +0100

    backup

--- argentbank-front/src/components/features/Features.jsx
+++ argentbank-front/src/components/features/Features.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Features({title, imageSrc, altText,content}) {
+export default function Features({ title, imageSrc, altText, content }) {
   return (
-        <div className="feature-item" key={title}>
-          <img src={imageSrc} alt={altText} className="feature-icon" />
-          <h3 className="feature-title">{title}</h3>
-          <p>{content}</p>
-        </div>
+    <div className="feature-item" key={title}>
+      <img src={imageSrc} alt={altText} className="feature-icon" />
+      <h3 className="feature-title">{title}</h3>
+      <p>{content}</p>
+    </div>
   );
 }
 
